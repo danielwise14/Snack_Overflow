2c5b0d35b1b2e09d9141ba4192b53e0ef4c5ca6d
Harsh Gupta <harshgupta2204@live.com>
2019-02-02 23:55:02 -0500

    Backend server with post requests works

--- ocr/script.py
+++ ocr/script.py
@@ -12,12 +12,15 @@ import numpy as np
 from google.cloud import vision
 from google.cloud.vision import types
 
-def draw_boxes(response, x_offset, y_offset):
-    im = np.array(Image.open('example3.jpg'), dtype=np.uint8)
-    # Create figure and axes
-    fig,ax = plt.subplots(1)
-    # Display the image
-    ax.imshow(im, origin='upper')
+def draw_boxes(response, x_offset, y_offset, img_path=None):
+    # Graphing Stuff(can be commented)
+    # -------------------------------------------------------
+    # im = np.array(Image.open(img_path), dtype=np.uint8)
+    # # Create figure and axes
+    # fig,ax = plt.subplots(1)
+    # # Display the image
+    # ax.imshow(im, origin='upper')
+    # -------------------------------------------------------
     items ={}
     price = []
     dot = False
@@ -41,13 +44,20 @@ def draw_boxes(response, x_offset, y_offset):
                         price[:] = []
                     else:
                         price.append((box.description))
-                    rect = patches.Rectangle((coords['x'],coords['y']),coords['w'], coords['h'],linewidth=1,edgecolor='r',facecolor='none')
-                    # Add the patch to the Axes
-                    ax.add_patch(rect)
+                    # Graphing
+                    # -------------------------------------------------------    
+                    # rect = patches.Rectangle((coords['x'],coords['y']),coords['w'], coords['h'],linewidth=1,edgecolor='r',facecolor='none')
+                    # # Add the patch to the Axes
+                    # ax.add_patch(rect)
+                    # -------------------------------------------------------
             else:
-                rect = patches.Rectangle((coords['x'],coords['y']),coords['w'], coords['h'],linewidth=1,edgecolor='g',facecolor='none')
-                # Add the patch to the Axes
-                ax.add_patch(rect)
+                # Graphing
+                # -------------------------------------------------------
+                # rect = patches.Rectangle((coords['x'],coords['y']),coords['w'], coords['h'],linewidth=1,edgecolor='g',facecolor='none')
+                # # Add the patch to the Axes
+                # ax.add_patch(rect)
+                # -------------------------------------------------------
+                pass
     # print(items)
     # print(len(items))
     hash_map = dict(zip(items.keys(), [[] for _ in range(len(items))]))
@@ -60,20 +70,18 @@ def draw_boxes(response, x_offset, y_offset):
     for box in response.text_annotations:
         coords = get_coords(box.bounding_poly.vertices)
         if coords['y']> y_offset[0] and coords['y']< y_offset[1] and coords['x'] < x_offset:
-            print(coords['y'])
-            # if d:
-            #     print(items)
-            #     d = False
             rval =is_in(coords, items.keys())
             if rval:
-                print("Found!", box.description)
                 hash_map[rval].append(box.description)
     final = []
     for k, v in hash_map.items():
         item_name = " ".join(v)
         final.append({item_name:items[k]})
-    print(final)
-    plt.show()
+    # print(final)
+    # Graphing
+    # -------------------------------------------------------
+    # plt.show()
+    # -------------------------------------------------------
     return final
     # return items
         
@@ -100,22 +108,33 @@ def init_conf():
     # Display the image
     ax.imshow(im, origin='upper')
     plt.show()
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'example3.jpg')
+def parse_receipt(x_offset, y_offset, image_url):
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+    # URI based web location image
+    # ------------------------------------------------
+    # image = vision.types.Image()
+    # image.source.image_uri = image_url
+    # ------------------------------------------------
+    # Local
+    # ------------------------------------------------
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        image_url)
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
 
-image = types.Image(content=content)
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
 
-# Performs label detection on the image file
-response = client.document_text_detection(image=image)
-# init_conf()
-# draw_boxes(response, 400, [50, 350])
-draw_boxes(response, 1800, [750, 1400])
+    image = types.Image(content=content)
+    # ------------------------------------------------
+
+    # Performs label detection on the image file
+    response = client.document_text_detection(image=image)
+    # init_conf()
+    # draw_boxes(response, 400, [50, 350])
+    # draw_boxes(response, 1800, [750, 1400])
+    return {'items':draw_boxes(response, x_offset, y_offset)}
